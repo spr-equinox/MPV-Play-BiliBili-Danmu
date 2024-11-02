39c52d9084934796bee3ff738a3c1bcae844d7d1
a2123228 <a2123228@qq.com>
2024-11-02 20:54:02 +0800

    Update load_danmu.py

--- load_danmu.py
+++ load_danmu.py
@@ -7,7 +7,12 @@ def main():
         exit(-1)
     cid = int(sys.argv[1])
     path = sys.argv[2]
-    response = request.urlopen(f'https://comment.bilibili.com/{cid}.xml', context=ssl.SSLContext(ssl.PROTOCOL_TLS))
+    response = request.urlopen(request.Request(
+        url = f'https://comment.bilibili.com/{cid}.xml',
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0'
+        }
+    ))
     data = str(zlib.decompress(response.read(), -zlib.MAX_WBITS), "utf-8")
     response.close()
     open(f'{path}/danmu.xml', 'w', encoding='utf-8').write(str(data))
